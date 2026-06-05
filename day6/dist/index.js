4147e31a0e485a17498b1db99e47091f4cef6b4a
Parichay Dutta Biswas <69819505+Parichay1811@users.noreply.github.com>
2026-06-05 19:10:20 +0530

    day6

--- day6/dist/index.js
+++ day6/dist/index.js
@@ -1,7 +1,60 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sum(a, b) {
-    return a + b;
+//Duck Typing / Structural Typing
+/* TypeScript does not care about the name of a type.
+
+It only cares about the structure (properties and methods).
+
+This is called Structural Typing (also known as Duck Typing).
+
+"If it looks like a duck and quacks like a duck, it's a duck." */
+/* type User = {
+    name: string,
+    age: number
 }
-console.log(sum(2, 9));
+
+const Person = {
+    name: "Parichay",
+    age: 25
+}
+
+const user:User = Person
+console.log(user); // This is valid because the structure of person matches with the user. all the properties of user are present in the Person.
+
+type Customer = {
+    name: string
+    id: number
+}
+
+const Consumer = {
+    name: "Parichay",
+    id:12334456,
+    age:23
+}
+const cus: Customer = Consumer // This is still valid because all the properties of Customer is present in Consumer.If any one of them would not be there it would throw new Error("");
+
+console.log(cus);
+
+
+//Real-world Example
+
+interface Animal {
+    sound(): void
+}
+
+class dog {
+    sound() {
+        console.log("woof!");
+    }
+}
+
+const animal: Animal = new dog()
+console.log(dog);
+
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const user = {
+    name: "Parichay",
+    email: "abc@gmail.com",
+};
+console.log(user);
 //# sourceMappingURL=index.js.map
--- day6/src/index.ts
+++ day6/src/index.ts
@@ -1,4 +1,153 @@
-function sum (a:number, b:number): number {
-    return a + b
-} 
-console.log(sum(2, 9));
+//Duck Typing / Structural Typing
+/* TypeScript does not care about the name of a type.
+
+It only cares about the structure (properties and methods).
+
+This is called Structural Typing (also known as Duck Typing).
+
+"If it looks like a duck and quacks like a duck, it's a duck." */
+/* type User = {
+    name: string,
+    age: number
+}
+
+const Person = {
+    name: "Parichay",
+    age: 25
+}
+
+const user:User = Person 
+console.log(user); // This is valid because the structure of person matches with the user. all the properties of user are present in the Person.
+
+type Customer = {
+    name: string
+    id: number
+}
+
+const Consumer = {
+    name: "Parichay",
+    id:12334456,
+    age:23
+}
+const cus: Customer = Consumer // This is still valid because all the properties of Customer is present in Consumer.If any one of them would not be there it would throw new Error("");
+
+console.log(cus);
+
+
+//Real-world Example
+
+interface Animal {
+    sound(): void
+}
+
+class dog {
+    sound() {
+        console.log("woof!");
+    }
+}
+
+const animal: Animal = new dog()
+console.log(dog);
+
+ */
+
+
+
+/* 
+
+// in the above if any one properties would be missing in the body then it would throw a new error. Which is a problem;
+
+// example:
+
+type Student = {
+    name: string,
+    age: number,
+    roll: number
+}
+const Stud = {
+    age:23
+}
+// const stu: Student = Stud //This will throw 'stu' is declared but its value is never read.
+// Type '{ age: number; }' is missing the following properties from type 'Student': name, roll
+
+//Here the Partial <T> comes into the picture it makes all the properties optional
+
+//Syntax: type PartialUser = Partial<User>;
+
+type Employee = {
+    name: string,
+    salary:number,
+    email:string
+}
+function updateEmployee(employee: Partial<Employee>) {
+    console.log(employee);
+}
+
+updateEmployee({
+    email: "abc@meta.co.in"
+}) // this is not throwing error because we made all the properties of updateEmployee optional using Partial <T>
+
+//Required <T>
+//syntax: type FullUser = Required<User>;
+
+interface User {
+  name?: string;
+  age?: number;
+}
+
+type FullUser = Required<User>
+//it means all the properties of user must be present in fullUser
+
+// const user: FullUser = {
+//   name: "Parichay",
+//   age: 23,
+// }; // valid
+
+const user: FullUser = {
+  name: "Parichay", //Throws error. because all the properties of User is not present at the  fullUser
+};
+
+ */
+
+/* 
+//Pick <T , k>
+//Pick perticular properties from a type 
+
+
+interface User {
+    id: number,
+    name: string
+    email: string,
+}
+
+type PublicUser = Pick <User, "id" | "name" >
+
+const user: PublicUser = {
+  id: 1,
+  name: "Parichay",
+};
+ */
+//Omit<T, K>
+/* 
+Opposite of Pick.
+Removes selected properties.
+
+ */
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+}
+
+
+type SafeUser = Omit<User, "id" | "password">
+
+const user: SafeUser = {
+  name: "Parichay",
+  email: "abc@gmail.com",
+};
+
+console.log(
+    user
+);
