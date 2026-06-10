cbd1bea2e0a9a199f249f7ffe59611802d354198
Parichay Dutta Biswas <69819505+Parichay1811@users.noreply.github.com>
2026-06-10 22:28:20 +0530

    day8

---
+++ day8/src/index.ts
@@ -0,0 +1,183 @@
+// console.log("Aap kaise Ho?");
+
+/* 
+let user = ["Parichay",25,true]  //javascript doesn't know that in the 1 position there is a number
+console.log(user[1]);
+ */
+
+//TypeScript Tuple
+// let user: [string, number, boolean];
+// user = ["Parichay", 25, true];
+
+//real world use case
+/* 
+let response: [number, string]
+response = [200, "success!"]
+
+enum Status {
+  SUCCESS,
+  FAILED,
+  LOADING
+}
+let currentStatus: Status = Status.SUCCESS;
+console.log(Status.LOADING);
+
+//Numeric Enum
+
+enum direction {
+    east,
+    west,
+    north,
+    south
+}
+console.log(direction.north);
+
+//String Enum
+
+// Most commonly used in projects.
+
+enum UserRole {
+  ADMIN = "ADMIN",
+  USER = "USER",
+  GUEST = "GUEST"
+}
+let role: UserRole = UserRole.ADMIN;
+enum OrderStatus {
+  PENDING = "PENDING",
+  SHIPPED = "SHIPPED",
+  DELIVERED = "DELIVERED"
+}
+
+function trackOrder(status: OrderStatus) {
+  console.log(status);
+}
+
+trackOrder(OrderStatus.SHIPPED);
+
+// Classes
+
+// class User{
+//     name: string
+//     constructor(name:string){
+//         this.name = name
+//     }
+// }
+
+
+// Multiple Properties
+class Student {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+const student = new Student("Parichay", 23);
+console.log(student);
+
+//Methods in Class
+class User {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  greet() {
+    console.log(`Hello ${this.name}`);
+  }
+}
+const user = new User("Parichay");
+
+user.greet(); //Hello Parichay
+
+
+ */
+
+//Constructors
+
+// Same like the last example
+
+
+//Shorthand Constructor
+
+// TypeScript provides a shorter syntax.
+class Employee {
+  constructor(
+    public name: string,
+    public salary: number
+  ) {}
+}
+
+//Equivalent to 
+// class Employee {
+//   name: string;
+//   salary: number;
+
+//   constructor(name: string, salary: number) {
+//     this.name = name;
+//     this.salary = salary;
+//   }
+// }
+
+// Access Modifiers (Public & Private)\
+
+//Public
+// class User {
+//   public name: string;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
+
+// const user = new User("Parichay");
+
+// console.log(user.name);
+
+//Private
+// class BankAccount {
+//   private balance: number = 1000;
+// }
+
+//We can not access the properties if this class
+
+class BankAccount {
+  private balance: number;
+
+  constructor(balance: number) {
+    this.balance = balance;
+  }
+
+  getBalance() {
+    return this.balance;
+  }
+}
+
+const account = new BankAccount(5000);
+
+console.log(account.getBalance());
+
+
+//Real world Example:
+class User {
+  public name: string;
+  private password: string;
+
+  constructor(name: string, password: string) {
+    this.name = name;
+    this.password = password;
+  }
+
+  checkPassword(password: string) {
+    return this.password === password;
+  }
+}
+
+const user = new User("Parichay", "12345");
+
+console.log(user.name);
+
+console.log(user.checkPassword("12345"));
